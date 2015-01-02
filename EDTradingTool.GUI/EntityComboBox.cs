6b97d3ce821ec45a061c278518cdc2b12b829c16
T <T@github.com>
2015-01-02 21:31:22 +0100

    Combo Boxes are now sorted automatically

--- EDTradingTool.GUI/EntityComboBox.cs
+++ EDTradingTool.GUI/EntityComboBox.cs
@@ -12,14 +12,13 @@ namespace EDTradingTool.GUI
     /// <summary>
     /// This class represents a combo box which automatically watches for Entity Events
     /// </summary>
-    public class EntityComboBox<T> : ComboBox, Core.IEntityWatcher<T>, IRequiresEntityHandler where T : Core.IEntity
+    public class EntityComboBox<T> : ComboBox, Core.IEntityWatcher<T>, IRequiresEntityHandler where T : Entity.EntityWithIdAndName
     {
         public EntityComboBox()
             : base()
         {
             this.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.DropDownStyle = ComboBoxStyle.DropDownList;
-            var thisAsComboBox = this as ComboBox;
             MinimumSize = new Size(100, 0);
         }
 
@@ -41,6 +40,14 @@ namespace EDTradingTool.GUI
         public void OnDataSetAdded(T dataSet, params Core.IEntity[] parentObjects)
         {
             Items.Add(dataSet);
+
+            Items.Remove(String.Empty);
+            var castedItems = Items.Cast<T>();
+            var orderedItems = castedItems.OrderBy(entity => entity.Name).ToArray();
+
+            Items.Clear();
+            Items.Add(String.Empty);
+            Items.AddRange(orderedItems);
         }
 
         public void OnDataSetUpdated(T dataSet)
